d745588c0779ed100a22fecd005bfa40d9b7e0f6
Daniel Nguyen <daniel.n.q.nguyen@gmail.com>
2024-11-27 22:53:52 +1100

    Scrape from adventofcode.com

---
+++ lib/advent_of_code_generator/scraper.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require "net/http"
+
+module AdventOfCodeGenerator
+  # Fetches puzzle descriptions and input data from adventofcode.com.
+  class Scraper
+    def initialize(year, day, session_key = nil)
+      @year = year
+      @day = day.to_i
+      @session_key = session_key
+    end
+
+    def puzzle_description
+      warn "You'll need to provide a session key to scrape Part Two." unless @session_key
+
+      uri = URI("https://adventofcode.com/#{@year}/day/#{@day}")
+      response = Net::HTTP.get_response(uri, headers)
+
+      response.body
+    end
+
+    def input_data
+      return @input_data if @input_data
+      return warn "No session key provided; unable to download data file." unless @session_key
+
+      uri = URI("https://adventofcode.com/#{@year}/day/#{@day}/input")
+      response = Net::HTTP.get_response(uri, headers)
+      @input_data ||= response.body
+    end
+
+    private
+
+    def headers
+      return {} unless @session_key
+
+      @headers ||= { cookie: "session=#{@session_key}" }
+    end
+  end
+end
---
+++ spec/advent_of_code_generator/scraper_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require "webmock/rspec"
+
+RSpec.describe AdventOfCodeGenerator::Scraper do
+  describe "#puzzle_description" do
+    before do
+      stub_request(:get, "https://adventofcode.com/2024/day/1")
+        .with(headers: { "Cookie" => "session=fake_session_key" })
+        .to_return(body: "<article>Test puzzle description</article>")
+    end
+
+    it "fetches the puzzle description from adventofcode.com" do
+      scraper = described_class.new(2024, 1, "fake_session_key")
+
+      expect(scraper.puzzle_description).to eq("<article>Test puzzle description</article>")
+    end
+  end
+
+  describe "#input_data" do
+    context "with a session key" do
+      before do
+        stub_request(:get, "https://adventofcode.com/2024/day/1/input")
+          .with(headers: { "Cookie" => "session=fake_session_key" })
+          .to_return(body: "1234\n5678\n")
+      end
+
+      it "fetches the input file from adventofcode.com" do
+        scraper = described_class.new(2024, 1, "fake_session_key")
+
+        expect(scraper.input_data).to eq("1234\n5678\n")
+      end
+
+      it "caches the response" do
+        scraper = described_class.new(2024, 1, "fake_session_key")
+
+        2.times { scraper.input_data }
+
+        expect(WebMock).to have_requested(:get, "https://adventofcode.com/2024/day/1/input").once
+      end
+    end
+
+    context "without a session key" do
+      it "returns nil" do
+        scraper = described_class.new(2024, 1)
+
+        expect(scraper.input_data).to be_nil
+      end
+
+      it "outputs a warning" do
+        scraper = described_class.new(2024, 1)
+
+        expect { scraper.input_data }.to output(/No session key/).to_stderr
+      end
+    end
+  end
+end
